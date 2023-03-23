e47d9f9484dd0124882a274836b234cb911900a4
mynhtt <mynhtt.cntt.hcmue@gmail.com>
2023-03-23 16:02:56 +0700

    update lab 2

--- src/com/rcv/readdata/ReadDataToTxtAndCSV.java
+++ src/com/rcv/readdata/ReadDataToTxtAndCSV.java
@@ -1,2 +1,19 @@
-package com.rcv.readdata;public class ReadDataToTxtAndCSV {
+package com.rcv.readdata;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadDataToTxtAndCSV {
+    public static void main(String[] args) throws IOException{
+        File f = new File("D:\\HCMUE\\2022-2023\\HK2\\KIEM THU PHAN MEM\\tesst\\src\\com\\rcv\\properties\\TestData.txt"); // đổi đuôi file từ txt thành csv để đổi loại file đọc
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        System.out.println(br.readLine());
+        System.out.println(br.readLine());
+        System.out.println(br.readLine());
+        System.out.println(br.readLine());
+        System.out.println("Read successfully");
+        br.close();
+    }
 }
